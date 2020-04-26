d594f038a10cacb3930320f56576b38c60f6c874
billy-zzz <63987064+billy-zzz@users.noreply.github.com>
2020-04-26 22:51:06 +1200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,64 @@ using UnityEngine;
 
 public class Player
 {
+    /// <summary>
+    /// stores the current position of the player
+    /// </summary>
     private Tile _playerPosition;
-
+    /// <summary>
+    /// stores the players account balance
+    /// </summary>
+    private decimal _playerBalance;
+    /// <summary>
+    /// stores all the properties owned by the player
+    /// </summary>
+    List<Property> _propertysOwned = new List<Property>();
+    /// <summary>
+    /// creates a player object
+    /// </summary>
+    /// <param name="playerPosition"></param>
     public Player (Tile playerPosition)
     {
         _playerPosition = playerPosition;
     }
-
+    /// <summary>
+    /// moves the player to the specified tile
+    /// </summary>
+    /// <param name="endPosition"></param>
     public void Move(Tile endPosition)
     {
-
+        _playerPosition = endPosition;
     }
-
+    /// <summary>
+    /// adds funds to the player
+    /// </summary>
+    /// <param name="Amount"></param>
+    public void AddFunds(decimal Amount)
+    {
+        _playerBalance += Amount;
+    }
+    /// <summary>
+    /// removes funds from the player
+    /// </summary>
+    /// <param name="Amount"></param>
+    public void RemoveFunds(decimal Amount)
+    {
+        _playerBalance -= Amount;
+    }
+    /// <summary>
+    /// adds a property to the players list of properties
+    /// </summary>
+    /// <param name="p"></param>
+    public void AddProperty(Property p)
+    {
+        _propertysOwned.Add(p);
+    }
+    /// <summary>
+    /// returns the players balance as a decimal
+    /// </summary>
+    /// <returns></returns>
+    public decimal GetBalance()
+    {
+        return _playerBalance;
+    }
 }
