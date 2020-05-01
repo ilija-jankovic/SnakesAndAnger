40330d5e27d57bbd58633a91831a06c17f00aadf
TGNanek <63989165+TGNanek@users.noreply.github.com>
2020-05-01 23:43:29 +1200

    Start of Card_Move class
    Class is for cards that make the player move when drawn

---
+++ Assets/Scripts/Card_Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card_Move : Card
+{
+    public Card_Move(byte id) :base (id)
+    {
+
+        Use();
+    }
+
+    //needs to have tiles / properties and movement finished to finish making
+
+    public override void Use()
+    {
+
+        //Player.Move(Player.playerPosition - 3);
+    }
+}
