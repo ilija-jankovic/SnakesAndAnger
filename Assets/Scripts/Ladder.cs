5a957d21fb51085089259980ed580c975f59b4cd
lewisvillav <lewis16.v@gmail.com>
2020-05-03 20:24:22 +1200

    Added Ladder Class, Start Banker Class, 1 Board Piece

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder
+{
+    private Tile _head;
+    private Tile _tail;
+
+    public Ladder(Tile head, Tile tail)
+    {
+        _head = head;
+        _tail = tail;
+    }
+
+    public Tile Head
+    {
+        get { return _head; }
+        set { _head = value; }
+    }
+
+    public Tile Tail
+    {
+        get { return _tail; }
+        set { _tail = value; }
+    }
+
+    public void DisplaySnake()
+    {
+        //displays snake
+    }
+
+    public void MovePlayer()
+    {
+        if (_head.hasPlayer())
+        {
+            _head.Player.Move(_head);
+        }
+
+    }
+
+
+}
