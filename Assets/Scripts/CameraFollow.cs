22da22be7ceb6352bbbd6def65291af97fbd11f0
billy-zzz <63987064+billy-zzz@users.noreply.github.com>
2020-05-13 13:54:15 +1200

    Camera now follows the hat object

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //object the camera will follow
+    public Transform target;
+    //speed the camera travels
+    public float smoothSpeed = 0.125f;
+    //offset of the camera
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        //allows a smooth follow of the object
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}
